5f24be08f6b735fe7327a84156dbe8186f2cc822
Pierre-Richard <mayer_alexandre@yahoo.fr>
2025-04-18 11:43:49 +0200

    added formModule for using two way binding and created openMenu function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,10 +2,26 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'em-header',
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    FormsModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isOpen: boolean = false;
+  /**
+   * Ouvre le sideMenu lorsque je clique sur le button
+   */
+  openMenu() {
+    this.isOpen = !this.isOpen;
+  }
+}
