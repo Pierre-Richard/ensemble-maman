cdba628abe75dee8c82983354f78ec20609c75ae
Pierre-Richard <mayer_alexandre@yahoo.fr>
2025-05-20 15:40:34 +0200

    used loop for in html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { CardsComponent } from '../cards/cards.component';
 import { CARD_UI_CONSTANT } from '../constants/constant';
 import { CommonModule } from '@angular/common';
-import { NgFor } from '@angular/common';
+
 @Component({
   selector: 'em-home',
-  imports: [CardsComponent, CommonModule, NgFor],
+  imports: [CardsComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
