6b2711c6b48e0b1d73c9d472514941c95171fc2b
Pierre-Richard <mayer_alexandre@yahoo.fr>
2025-05-25 23:17:55 +0200

    added loop for in home component html

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardsComponent } from '../../shared/components/cards/cards.component';
 import { CARD_UI_CONSTANT } from '../../shared/constants/card_Ui_Constant';
+import { CarouselComponent } from '../../shared/components/carousel/carousel.component';
 
 @Component({
   selector: 'em-home',
-  imports: [CardsComponent, CommonModule],
+  imports: [CardsComponent, CommonModule, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
