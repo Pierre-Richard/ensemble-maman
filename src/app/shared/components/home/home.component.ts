da1d5a9c662be4113f7cdcd75dc3c11daa4da6bb
Pierre-Richard <mayer_alexandre@yahoo.fr>
2025-05-26 15:20:41 +0200

    removed import cardsComponent from home component

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CardsComponent } from '../../../shared/components/cards/cards.component';
 import { CARD_UI_CONSTANT } from '../../../shared/constants/card_Ui_Constant';
 import { CommonModule } from '@angular/common';
 
@@ -7,7 +6,7 @@ import { CarouselComponent } from '../../../shared/components/carousel/carousel.
 
 @Component({
   selector: 'em-home',
-  imports: [CardsComponent, CommonModule, CarouselComponent],
+  imports: [CommonModule, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
