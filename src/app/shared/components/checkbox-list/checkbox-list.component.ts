84dcc33a7487db78941f28f29be1c28270f4cf58
Pierre-Richard <mayer_alexandre@yahoo.fr>
2025-05-28 10:11:06 +0200

    created  interface checkbox

--- src/app/shared/components/checkbox-list/checkbox-list.component.ts
+++ src/app/shared/components/checkbox-list/checkbox-list.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Checkbox } from '../../interfaces/Checkbox';
 
 @Component({
   selector: 'em-checkbox-list',
   imports: [],
   templateUrl: './checkbox-list.component.html',
-  styleUrl: './checkbox-list.component.scss'
+  styleUrl: './checkbox-list.component.scss',
 })
 export class CheckboxListComponent {
-
+  // si possible faire de task un signal
+  task: Checkbox[] = [
+    {
+      name: '1an',
+      completed: false,
+    },
+  ];
 }
