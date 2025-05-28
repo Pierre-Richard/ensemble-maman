3cf5f4886f649aa9aa5d31986b0f69199aa475d3
Pierre-Richard <mayer_alexandre@yahoo.fr>
2025-05-28 09:53:00 +0200

    create component checkboxList

---
+++ src/app/shared/components/checkbox-list/checkbox-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'em-checkbox-list',
+  imports: [],
+  templateUrl: './checkbox-list.component.html',
+  styleUrl: './checkbox-list.component.scss'
+})
+export class CheckboxListComponent {
+
+}
