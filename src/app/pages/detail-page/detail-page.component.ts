9e184a9871e6b8c2d8d983d58bf4e9979bfbe127
Pierre-Richard <mayer_alexandre@yahoo.fr>
2025-06-02 08:29:42 +0200

    import checkboxlist in detail page component

--- src/app/pages/detail-page/detail-page.component.ts
+++ src/app/pages/detail-page/detail-page.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Card } from '../../shared/interfaces/Card';
 import { CardsListComponent } from '../../shared/components/cards-list/cards-list.component';
 import { CARD_UI_CONSTANT } from '../../shared/constants/card-Ui.constant';
 import { CheckboxListComponent } from '../../shared/components/checkbox-list/checkbox-list.component';
 import { CHECKBOX_TASK_CONSTANT } from '../../shared/constants/checkbox-task.constant';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  FormControl,
+  FormControlName,
+} from '@angular/forms';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CheckboxLabel } from '../../shared/interfaces/CheckboxLabel';
+
+//1 creer interface pour CheckboxLabelUi
+// 2 mettre le form dans le la page checkobx et importer le composant checklist dans le detail compoent et que tout marche
 
 @Component({
   selector: 'em-detail-page',
-  imports: [CheckboxListComponent],
+  imports: [ReactiveFormsModule, MatCheckboxModule, CheckboxListComponent],
   templateUrl: './detail-page.component.html',
   styleUrl: './detail-page.component.scss',
 })
 export class DetailPageComponent {
-  task = CHECKBOX_TASK_CONSTANT;
+  test = [];
 }
--- src/app/shared/components/checkbox-list/checkbox-list.component.ts
+++ src/app/shared/components/checkbox-list/checkbox-list.component.ts
@@ -1,14 +1,43 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { Checkbox } from '../../interfaces/Checkbox';
-import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CheckboxLabel } from '../../interfaces/CheckboxLabel';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+const CheckboxLabelUi: CheckboxLabel = {
+  ['oneToTwelve']: '1 à 12 mois',
+  ['oneToThree']: '1 à 3 ans',
+  ['fourToSixth']: '4 à 6 ans',
+  ['sevenToNine']: '7 à 9 ans',
+  ['nineAndMore']: '9 et plus',
+};
 @Component({
   selector: 'em-checkbox-list',
-  imports: [MatCheckboxModule],
+  imports: [ReactiveFormsModule, MatCheckboxModule],
   templateUrl: './checkbox-list.component.html',
   styleUrl: './checkbox-list.component.scss',
 })
 export class CheckboxListComponent {
-  // si possible faire de task un signal
-  task = input.required<Checkbox[]>();
+  task = input.required<string[]>();
+
+  tasks = signal<string[]>([]);
+  checkboxLabelUi = CheckboxLabelUi;
+
+  private formBuilder = inject(FormBuilder);
+
+  sizeForm = this.formBuilder.group({
+    oneToTwelve: [false],
+    oneToThree: [false],
+    fourToSixth: [false],
+    sevenToNine: [false],
+    nineAndMore: [false],
+  });
+
+  ngOnInit() {
+    this.sizeForm.valueChanges.subscribe((x) => {
+      console.log('sizeForm', x);
+    });
+    console.log('Value', Object.keys(this.sizeForm.value));
+    this.tasks.set(Object.keys(this.sizeForm.value));
+  }
 }
